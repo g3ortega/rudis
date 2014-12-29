fe19ed754a28b615f8e78cb835c2743cda0cb7f1
Gerardo <geraldavid7@gmail.com>
2014-12-28 21:08:20 -0600

    non-blocking parser

--- lib/rudis/server.rb
+++ lib/rudis/server.rb
@@ -9,46 +9,108 @@ module Rudis
 
     def listen
       readable = []
+      clients = {}
       server = TCPServer.new(port)
       readable << server
       loop do
-        ready_to_read, _ = IO.select(readable)
+        ready_to_read, _ = IO.select(readable + clients.keys)
 
         ready_to_read.each do |socket|
           case socket
             when server
-              readable << socket.accept
+              child_socket = socket.accept
+              clients[child_socket] = Handler.new(child_socket)
             else
-              handle_client socket
+              begin
+                clients[socket].process!
+              rescue EOFError
+                clients.delete(socket)
+                socket.close
+              end
           end
-
         end
       end
     ensure
-      readable.each do |socket|
+      (readable + clients.keys).each do |socket|
         socket.close
       end
     end
 
-    def handle_client(client)
-      header = client.gets.to_s
+    class Handler
+      attr_reader :client
 
-      return unless header[0] == '*'
-
-      num_args = header[1..-1].to_i
-
-      cmd = num_args.times.map do
-        len = client.gets[1..-1].to_i
-        client.read(len + 2).chomp
+      def initialize(socket)
+        @client = socket
+        @buffer = ""
       end
 
-      response = case cmd[0].downcase
-                   when 'ping' then "+PONG\r\n"
-                   when 'echo' then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
-                 end
+      def process!
+        buffer << client.read_nonblock(10)
 
-      client.write response
+        cmds, processed = unmarshal(buffer)
+
+        @buffer = buffer[processed..-1]
+
+        cmds.each do |cmd|
+          response = case cmd[0].downcase
+          when 'ping' then "+PONG\r\n"
+          when 'echo' then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
+          end
+
+          client.write response
+        end
+      end
+
+      class ProtocolError < RuntimeError; end
+
+      def unmarshal(data)
+        io = StringIO.new(data)
+        result = []
+        processed = 0
+
+      begin
+        loop do
+          header = safe_readline(io)
+
+          raise ProtocolError unless header[0] == '*'
+
+          n = header[1..-1].to_i
+
+          result << n.times.map do
+            raise ProtocolError unless io.readpartial(1) == '$'
+
+            length = safe_readline(io).to_i
+            safe_readpartial(io, length).tap do
+              safe_readline(io)
+            end
+          end
+
+          processed = io.pos
+        end
+        rescue ProtocolError
+          processed = io.pos
+        rescue EOFError
+          # Incomplete command, ignore
+      end
+
+        [result, processed]
+      end
+
+      def safe_readline(io)
+        io.readline("\r\n").tap do |line|
+          raise EOFError unless line.end_with?("\r\n")
+        end
+      end
+
+      def safe_readpartial(io, length)
+        io.readpartial(length).tap do |data|
+          raise EOFError unless data.length == length
+        end
+      end
+
+
     end
+
 
     private
 
